cf7e7fccbadb44ccfae0dee0cc476f35cd83818b
Ali Abbas <aliabbas.299792@gmail.com>
2021-08-04 22:16:24 +0200

    Style changes (works better on mobile browsers now)

--- public/assets/index.js
+++ public/assets/index.js
@@ -530,6 +530,7 @@ window.addEventListener('click', e => {
 })
 
 const vote_modal_tracks = document.getElementById("vote_tracks");
+const vote_modal_header = document.getElementById("vote_modal_header");
 
 document.getElementById("vote_btn").onclick = refresh_voted_for_stuff
 
@@ -549,6 +550,8 @@ function refresh_voted_for_stuff(data = undefined) {
         el.classList.add("vote_track_item");
       }
     }
+
+    vote_modal_header.style.width = `${vote_modal_tracks.offsetWidth}px`; // makes sure the header is the correct size
   }
 
   if(typeof data ==	"string" && data != ""){
